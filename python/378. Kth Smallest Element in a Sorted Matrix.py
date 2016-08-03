56a22f3fda49015fbf332c7e8dc67e4b0d6df7a4
CrazyCoder4Carrot <liuzhenbang1988@gmail.com>
2016-08-02 20:09:27 -0400

    378. Kth Smallest Element in a Sorted Matrix

---
+++ python/378. Kth Smallest Element in a Sorted Matrix.py
@@ -0,0 +1,9 @@
+import heapq
+class Solution(object):
+    def kthSmallest(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        return list(heapq.merge(*matrix))[k-1]
