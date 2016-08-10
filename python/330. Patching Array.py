b4d12b40039e6895b140851d2e659dd2306f7548
CrazyCoder4Carrot <liuzhenbang1988@gmail.com>
2016-08-10 11:34:16 -0400

    330. Patching Array

---
+++ python/330. Patching Array.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def minPatches(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: int
+        """
+        miss, add, i = 1, 0, 0
+        while miss <= n:
+            if i < len(nums) and nums[i] <= miss:
+                miss += nums[i]
+                i += 1
+            else:
+                miss += miss
+                add += 1
+        return add
+                
