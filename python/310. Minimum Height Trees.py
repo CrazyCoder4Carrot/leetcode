9883bc1c56faa321c3c6b683d86a187e4cf0f4d1
CrazyCoder4Carrot <liuzhenbang1988@gmail.com>
2016-11-23 15:24:16 -0500

    310. Minimum Height Trees

---
+++ python/310. Minimum Height Trees.py
@@ -0,0 +1,34 @@
+import collections
+import sys
+class Solution(object):
+    def findMinHeightTrees(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: List[int]
+        """
+        def dfs(root, distance, level):
+        	level += 1
+        	for node in graph[root]:
+        		distance[node] = level
+        		visit[node] = True
+        		if not visit[node]:
+        			dfs(node, distance)
+        if not edges:
+        	return []
+        graph = collections.defaultdict(list)
+        for a,b in edges:
+        	graph[a].append(b) 
+        	graph[b].append(a)
+        vetex = set(graph.keys())
+        res = []
+        while len(vetex) > 2:
+        	leaves = [x for x in graph if len(graph[x]) == 1]
+        	for x in leaves:
+        		for y in graph[x]:
+        			graph[y].remove(x)
+        		del graph[x]
+        		vetex.remove(x)
+       	return list(vetex)
+sol = Solution()
+print sol.findMinHeightTrees(4,[[1,0],[1,2],[1,3]])
