13389277bac6a9011fa90d77d19003b36a037f25
CrazyCoder4Carrot <liuzhenbang1988@gmail.com>
2016-11-28 18:47:03 -0500

    update

--- python/210. Course Schedule II.py
+++ python/210. Course Schedule II.py
@@ -1,18 +1,20 @@
 import collections
 class Solution(object):
-    def canFinish(self, n, prerequisites):
+    def findOrder(self, n, prerequisites):
         """
         :type numCourses: int
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
         graph = {}
+        res = []
         for a, b in prerequisites:
             if a in graph and b not in graph[a]:
                 graph[a].append(b)
             else:
                 graph[a] = [b]
         remove_list = [i for i in range(n) if i not in graph] 
+        res += remove_list
         while remove_list:
             cur_list = [key for key in graph]
             temp = []
@@ -25,9 +27,8 @@ class Solution(object):
                 for key in temp:
                     if key in graph:
                         del graph[key]
-            remove_list = [i for i in cur_list if i not in graph] 
-        return len([key for key in graph]) == 0
-
-
+            remove_list = [i for i in cur_list if i not in graph]
+            res += remove_list
+        return res if not graph else []
 sol = Solution()
-sol.canFinish(2, [[1,0]])
+sol.findOrder(2, [[1,0]])
