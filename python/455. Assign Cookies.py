e6634c3ba5ce92e0bf3bec014fcfe4cfab858285
Zhenbang Liu <liuzhenbang1988@gmail.com>
2016-12-25 15:40:49 -0500

    455. Assign Cookies

---
+++ python/455. Assign Cookies.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+        i = j = 0
+        g.sort()
+        s.sort()
+        count = 0
+        while i < len(g) and j < len(s):
+            if g[i] <= s[j]:
+                i += 1
+                j += 1
+                count += 1
+                continue
+            if g[i] > s[j]:
+                j+= 1
+                continue
+        return count
+                
+                
