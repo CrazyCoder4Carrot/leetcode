5c653c5e9141d8cd71e4d8b4906aa9159c6ff0d5
CrazyCoder4Carrot <liuzhenbang1988@gmail.com>
2017-01-03 18:07:36 -0500

    424. Longest Repeating Character Replacement

--- python/424. Longest Repeating Character Replacement.py
+++ python/424. Longest Repeating Character Replacement.py
@@ -5,21 +5,19 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        i = j = 0 
-        res = 0
-        cur = ""
-        while i < len(s):
-        	length = 1
-        	cur = s[i]
-        	temp = 0
-        	for j in range(i+1, len(s)):
-        		if s[j] != cur:
-        			temp += 1
-        		if temp > k:
-        			break
-        		length += 1
-        	i += length
-        	res = max(res, length)
+        res, low = 0, 0
+        dict = {}
+        for i in range(len(s)):
+            if s[i] not in dict:
+                dict[s[i]] = 1
+            else:
+                dict[s[i]] += 1
+            count = sum(dict.values()) - max(dict.values())
+            while count > k and low <= i:
+                dict[s[low]] -= 1
+                if dict[s[low]] == 0:
+                    del dict[s[low]]
+                low += 1
+                count = sum(dict.values()) - max(dict.values())
+            res = max(res, i - low + 1)
         return res
-sol = Solution()
-print sol.characterReplacement("ABBB", 2)
