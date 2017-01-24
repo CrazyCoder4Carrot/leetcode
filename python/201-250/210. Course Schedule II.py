d02be860be73785cc95d47c0abc834b3b91fbd13
Zhenbang Liu <liuzhenbang1988@gmail.com>
2017-01-23 23:20:56 -0500

    update

--- python/201-250/210. Course Schedule II.py
+++ python/201-250/210. Course Schedule II.py
@@ -1,34 +1,35 @@
 import collections
 class Solution(object):
-    def findOrder(self, n, prerequisites):
+    def findOrder(self, numCourses, prerequisites):
         """
         :type numCourses: int
         :type prerequisites: List[List[int]]
-        :rtype: bool
+        :rtype: List[int]
         """
-        graph = {}
-        res = []
-        for a, b in prerequisites:
-            if a in graph and b not in graph[a]:
-                graph[a].append(b)
-            else:
-                graph[a] = [b]
-        remove_list = [i for i in range(n) if i not in graph] 
-        res += remove_list
-        while remove_list:
-            cur_list = [key for key in graph]
-            temp = []
-            for i in remove_list:
-                for key in graph:
-                    if i in graph[key]:
-                        graph[key].remove(i)
-                        if not graph[key]:
-                            temp.append(key)
-                for key in temp:
-                    if key in graph:
-                        del graph[key]
-            remove_list = [i for i in cur_list if i not in graph]
-            res += remove_list
-        return res if not graph else []
+        graph = collections.defaultdict(list)
+        firstlevel = [True] * numCourses
+        for pair in prerequisites:
+            firstlevel[pair[0]] = False
+            graph[pair[1]].append(pair[0])
+        visited = collections.defaultdict(bool)
+        stack = []
+        def dfs(cur):
+            if len(graph[cur]) == 0:
+                stack.append(cur)
+                return 
+            for node in graph[cur]:
+                print node
+                if not visited[node]:
+                    visited[node] = True
+                    dfs(node)
+            stack.append(cur)
+        for i in range(numCourses):
+            if firstlevel[i]:
+                visited[i] = True
+                dfs(i)
+        return stack[::-1]
+
 sol = Solution()
-sol.findOrder(2, [[1,0]])
+num = 4
+pre = [[1,0],[2,1],[3,2],[1,3]]
+print sol.findOrder(num, pre)
