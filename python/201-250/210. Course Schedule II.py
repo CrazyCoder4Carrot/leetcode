76614b5c14a71828d3299a7d1c6e884790121c7e
CrazyCoder4Carrot <liuzhenbang1988@gmail.com>
2017-01-23 20:31:48 -0500

    210. Course Schedule II

--- python/201-250/210. Course Schedule II.py
+++ python/201-250/210. Course Schedule II.py
@@ -1,34 +1,35 @@
 import collections
+
+
 class Solution(object):
-    def findOrder(self, n, prerequisites):
+    def findOrder(self, numCourses, prerequisites):
         """
         :type numCourses: int
         :type prerequisites: List[List[int]]
-        :rtype: bool
+        :rtype: List[int]
         """
-        graph = {}
+        dic = collections.defaultdict(list)
+        graph = collections.defaultdict(list)
+        for i, j in prerequisites:
+            dic[i].append(j)
+            graph[j].append(i)
+        stack = [i for i in xrange(numCourses) if not dic[i]]
         res = []
-        for a, b in prerequisites:
-            if a in graph and b not in graph[a]:
-                graph[a].append(b)
-            else:
-                graph[a] = [b]
-        remove_list = [i for i in range(n) if i not in graph] 
-        res += remove_list
-        while remove_list:
-            cur_list = [key for key in graph]
-            temp = []
-            for i in remove_list:
-                for key in graph:
-                    if i in graph[key]:
-                        graph[key].remove(i)
-                        if not graph[key]:
-                            temp.append(key)
-                for key in temp:
-                    if key in graph:
-                        del graph[key]
-            remove_list = [i for i in cur_list if i not in graph]
-            res += remove_list
-        return res if not graph else []
+        while stack:
+            node = stack.pop()
+            res.append(node)
+            for i in graph[node]:
+                dic[i].remove(node)
+                if not dic[i]:
+                    stack.append(i)
+            del dic[node]
+        return res if not dic else []
+
+
+
+
+
 sol = Solution()
-sol.findOrder(2, [[1,0]])
+num = 2
+pre = [[1, 0]]
+print sol.findOrder(num, pre)
