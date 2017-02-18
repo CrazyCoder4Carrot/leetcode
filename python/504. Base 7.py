c32fd021c9454f4e2d8351631d9ae56c5d70a1a6
CrazyCoder4Carrot <liuzhenbang1988@gmail.com>
2017-02-18 16:22:35 -0500

    504. Base 7

---
+++ python/504. Base 7.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def convertToBase7(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        if not num:
+            return "0"
+        flag = False
+        if num < 0:
+            flag = True
+            num = -num
+        res = ""
+        while num:
+            res = str(num % 7) + res
+            num /= 7
+        if flag:
+            res = "-" + res
+        return res
+        
