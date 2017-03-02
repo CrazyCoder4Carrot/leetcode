4ced96e015b4fba263814726a17ba1eb684dbb79
CrazyCoder4Carrot <liuzhenbang1988@gmail.com>
2017-03-02 15:29:05 -0500

    7. Reverse Integer

--- cpp/1-50/7. Reverse Integer.cpp
+++ cpp/1-50/7. Reverse Integer.cpp
@@ -1,6 +1,33 @@
-class Solution {
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <climits>
+using namespace std;
+class Solution
+{
 public:
-    int reverse(int x) {
-        
-    }
+        int reverse(int x)
+        {
+                if (!x)
+                        return 0;
+                int flag = 1;
+                if (x < 0) {
+                        flag = -1;
+                        x = -x;
+                }
+                long res = 0;
+                while (x)
+                {
+                        res = res * 10 + x % 10;
+                        x /= 10;
+                }
+                res *= flag;
+                return (res > INT_MAX || res < INT_MIN) ? 0 : res;
+        }
 };
+
+int main()
+{
+        Solution sol;
+        cout << sol.reverse(100) << endl;
+}
