7cbe5c75235941ed11a6a0a97636c5274ac5aa6c
CrazyCoder4Carrot <liuzhenbang1988@gmail.com>
2017-03-04 18:25:37 -0500

    24. Swap Nodes in Pairs

--- cpp/1-50/24. Swap Nodes in Pairs.cpp
+++ cpp/1-50/24. Swap Nodes in Pairs.cpp
@@ -11,26 +11,29 @@ public:
     ListNode* swapPairs(ListNode* head) {
         if(head == NULL)
             return NULL;
-        if(head->next = NULL)
+        if(head->next == NULL)
             return head;
         ListNode *dummyhead = new ListNode(0);
         dummyhead->next = head;
+        ListNode *ptr = dummyhead;
+        while(ptr)
+        {
+            cout<<ptr->val<<" ";
+            ptr = ptr->next;
+        }
         ListNode *ptr1 = dummyhead;
         ListNode *ptr2 = head;
         ListNode *ptr3 = ptr2->next;
+        cout<<head->next<<endl;
         while(ptr1 && ptr2 && ptr3)
         {
-            swap(ptr1, ptr2, ptr3);
+            ptr1->next = ptr3;
+            ptr2->next = ptr3->next;
+            ptr3->next = ptr2;
             ptr1 = ptr2;
             ptr2 = ptr2->next;
             ptr3 = ptr2? ptr2->next : NULL;
         }
         return dummyhead->next;
-    }
-    void swap(ListNode *p1, ListNode *p2, ListNode *p3)
-    {
-        p1->next = p3;
-        p2->next = p3->next;
-        p3->next = p2;
     }
 };
