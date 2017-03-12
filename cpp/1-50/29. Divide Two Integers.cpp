08616b6e2fea41918ec516ec097b041e61e52040
CrazyCoder4Carrot <liuzhenbang1988@gmail.com>
2017-03-11 20:29:17 -0500

    29. Divide Two Integers

---
+++ cpp/1-50/29. Divide Two Integers.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+	int divide(long dividend, long divisor) {
+		int flag = false;
+		if (dividend < INT_MIN || dividend > INT_MAX)
+			return INT_MAX;
+		if (divisor < INT_MIN || divisor > INT_MAX)
+			return INT_MAX;
+		if (dividend < 0){
+			flag = !flag;
+			dividend = -dividend;
+		}
+		if (divisor < 0){
+			flag = !flag;
+			divisor = -divisor;
+		}
+		long res = 0;
+		while(dividend != 0  || dividend >= divisor){
+			long temp = 1;
+			long div = divisor;
+			while(div < dividend){
+				div <<= 1;
+				temp <<= 1;
+			}
+			if(div > dividend)
+			{
+				div >>= 1;
+				temp>>= 1;
+			}
+			dividend -= div;
+			res += temp;
+		}
+		res = flag? -res: res;
+		if(res < INT_MIN || res > INT_MAX){
+			return INT_MAX;
+		}
+		else
+			return res;
+	}
+};
