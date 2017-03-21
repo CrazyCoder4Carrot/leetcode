97da1366c177f1d7503fffb338eb9538a11fc047
CrazyCoder4Carrot <liuzhenbang1988@gmail.com>
2017-03-20 21:19:13 -0400

    65. Valid Number

--- cpp/51-100/64. Minimum Path Sum.cpp
+++ cpp/51-100/64. Minimum Path Sum.cpp
@@ -8,6 +8,8 @@ public:
 			return 0;
 		for(int j = 1; j < n; j++)
 			grid[0][j] += grid[0][j-1];
+		for(int i = 1; i < m; i++)
+			grid[i][0] += grid[i-1][0];
 		for(int i = 1; i <= m; i++)
 		{
 			for(int j = 1; j <= n; j++)
--- cpp/51-100/65. Valid Number.cpp
+++ cpp/51-100/65. Valid Number.cpp
@@ -2,28 +2,29 @@ class Solution {
 public:
 	bool isNumber(string s) {
 		int i = 0;
-		while (s[i] == ' ')
+		while (s[0] == ' ')
 			s.erase(0, 1);
-		
 		while (s[s.size() - 1] == ' ')
 			s.erase(s.size() - 1, 1);
 		if (s.empty())
 			return false;
-		if (s[i] == '+' || s[i] == '-')
+		int n_num = 0, n_dot = 0;
+		if (s[i] == '-' || s[i] == '+')
 			i++;
-		int n_number = 0, n_dot = 0;
-		for (; ((s[i] <= '9' && s[i] >= '0') || s[i] == '.'); i++)
-			s[i] == '.' ? n_dot++ : n_number++;
-		if (n_dot > 1 || n_number < 1)
+		for (; (s[i] >= '0' && s[i] <= '9') || s[i] == '.' ; i++) {
+			s[i] == '.' ? n_dot++ : n_num++;
+		}
+		if (n_num < 1 || n_dot > 1)
 			return false;
 		if (s[i] == 'e') {
 			i++;
-			if (s[i] == '+' || s[i] == '-' ) i++;
-			n_number = 0;
-			for (; s[i] <= '9' && s[i] >= '0'; i++, n_number++){}
-			if (n_number < 1)
-				return false;
-		}
+			if (s[i] == '-' || s[i] == '+')
+				i++;
+			n_num = 0;
+			for (; s[i] <= '9' && s[i] >= '0'; i++, n_num++) {}
+			if (n_num < 1)
+				return false
+			}
 		return s[i] == 0;
 	}
 };
