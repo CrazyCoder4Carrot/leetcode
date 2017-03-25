469f9ca8280c839e8d685e03ce36601924faec45
CrazyCoder4Carrot <liuzhenbang1988@gmail.com>
2017-03-25 17:57:55 -0400

    72. Edit Distance

--- cpp/51-100/72. Edit Distance.cpp
+++ cpp/51-100/72. Edit Distance.cpp
@@ -10,14 +10,15 @@ public:
 			dp.push_back(row);
 		}
 		for(int i = 1; i <= word2.size(); i++)
-			dp[0][i] = 1;
+			dp[0][i] = i;
 		for(int i = 1; i <= word1.size(); i++)
-			dp[i][0] = 1;
+			dp[i][0] = i;
 		for(int i = 1; i <= word1.size(); i++)
 			for(int j = 1; j <= word2.size(); j++){
-				dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]);
-				if(word1[i-1] != word2[j-1])
-					dp[i][j]++;
+				if(word1[i-1] == word2[j-1])
+					dp[i][j] = dp[i-1][j-1];
+				else
+					dp[i][j] = min(dp[i-1][j], min(dp[i][j-1], dp[i-1][j-1])) + 1;
 			}
 		return dp[word1.size()][word2.size()];
 	}
