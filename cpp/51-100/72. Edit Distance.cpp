82005be5fa0aa47062100272d889dada9aa3e979
CrazyCoder4Carrot <liuzhenbang1988@gmail.com>
2017-03-25 17:24:01 -0400

    72. Edit Distance

---
+++ cpp/51-100/72. Edit Distance.cpp
@@ -0,0 +1,24 @@
+#include<string>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+	int minDistance(string word1, string word2) {
+		vector<vector<int> > dp;
+		for(int i = 0 ; i <= word1.size(); i++){
+			vector<int> row(word2.size() + 1, 0);
+			dp.push_back(row);
+		}
+		for(int i = 1; i <= word2.size(); i++)
+			dp[0][i] = 1;
+		for(int i = 1; i <= word1.size(); i++)
+			dp[i][0] = 1;
+		for(int i = 1; i <= word1.size(); i++)
+			for(int j = 1; j <= word2.size(); j++){
+				dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]);
+				if(word1[i-1] != word2[j-1])
+					dp[i][j]++;
+			}
+		return dp[word1.size()][word2.size()];
+	}
+};
