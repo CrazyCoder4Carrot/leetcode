c3869043d202547e885f9eee1b1259978fba8f3f
CrazyCoder4Carrot <liuzhenbang1988@gmail.com>
2017-04-03 19:59:30 -0400

    95. Unique Binary Search Trees II

--- cpp/51-100/95. Unique Binary Search Trees II.cpp
+++ cpp/51-100/95. Unique Binary Search Trees II.cpp
@@ -10,6 +10,29 @@
 class Solution {
 public:
 	vector<TreeNode*> generateTrees(int n) {
-
+	    vector<TreeNode *> res;
+	    if(n == 0)
+	    return res;
+		return generator(1, n);
+	}
+	vector<TreeNode *> generator(int s, int e) {
+		vector<TreeNode *> res;
+		if (s > e) {
+		    res.push_back(NULL);
+			return res;
+		}
+		for (int i = s; i <= e; i++) {
+			vector<TreeNode *> left_nodes = generator(s, i - 1);
+			vector<TreeNode *> right_nodes = generator(i + 1, e);
+			for(auto left: left_nodes)
+				for(auto right: right_nodes)
+				{
+					TreeNode *root = new TreeNode(i);
+					root->left = left;
+					root->right = right;
+					res.push_back(root);
+				}
+		}
+		return res;
 	}
 };
