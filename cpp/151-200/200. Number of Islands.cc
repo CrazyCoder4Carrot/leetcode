7fb2d659f739b0cc7b3e76150dfc41b1fcfd9cde
CrazyCoder4Carrot <liuzhenbang1988@gmail.com>
2017-07-31 15:18:04 -0400

    200. Number of Islands

---
+++ cpp/151-200/200. Number of Islands.cc
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        if (grid.empty())
+            return 0;
+        int res = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == '1') {
+                    bfs(grid, i, j);
+                    res ++;
+                }
+            }
+        }
+        return res;
+    }
+    void bfs(vector<vector<char>> &grid, int i, int j) {
+        if (i < 0 || i >= grid.size())
+            return;
+        if (j < 0 || j >= grid[0].size())
+            return;
+        queue<pair<int, int>> q;
+        q.push(make_pair(i, j));
+        while (!q.empty()) {
+            pair<int, int> p = q.front();
+            q.pop();
+            int x, y = p.first, p.last;
+            if(x < 0 || x >= grid.size() || y < 0 || y >= grid[0].size() || grid[x][y] == '0')
+            	continue;
+            grid[x][y] = '0';
+            q.push(make_pair(x - 1, y));
+            q.push(make_pair(x + 1, y));
+            q.push(make_pair(x, y + 1));
+            q.push(make_pair(x, y - 1));
+        }
+    }
+};
