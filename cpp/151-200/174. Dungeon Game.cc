437ffb039fb649a729eeb05672b5d71c638aaaf1
CrazyCoder4Carrot <liuzhenbang1988@gmail.com>
2017-08-21 19:53:15 -0400

    174. Dungeon Game

--- cpp/151-200/174. Dungeon Game.cc
+++ cpp/151-200/174. Dungeon Game.cc
@@ -11,8 +11,9 @@ public:
         for (int i = m - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
             	int need = min(dp[i+1][j], dp[i][j+1]) - dungeon[i][j];
-            	dp[i][j] = 
+            	dp[i][j] = need <= 0? 1: need;
             }
         }
+        return dp[0][0];
     }
 };
